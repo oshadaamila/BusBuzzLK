f4d8503b3d5e68733eb2a536b88fbf891973ccee
HeshanSudarshana <heshan.15@cse.mrt.ac.lk>
2019-08-04 00:21:25 +0530

    validation added

--- app/src/main/java/com/crystalit/busbuzzlk/Fragments/SignInFragment.java
+++ app/src/main/java/com/crystalit/busbuzzlk/Fragments/SignInFragment.java
@@ -6,18 +6,21 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.crystalit.busbuzzlk.Components.InputValidator;
 import com.crystalit.busbuzzlk.R;
 import com.crystalit.busbuzzlk.ViewModels.SignInViewModel;
 
 public class SignInFragment extends Fragment {
 
     private SignInViewModel mViewModel;
+    private InputValidator inputValidator;
 
     //declaring view components
     EditText uNameET, passwordET;
@@ -33,7 +36,7 @@ public class SignInFragment extends Fragment {
         View fragmentView = inflater.inflate(R.layout.sign_in_fragment, container, false);
 
         // initializing view components
-        uNameET = fragmentView.findViewById(R.id.email_edit_text);
+        uNameET = fragmentView.findViewById(R.id.username_edit_text);
         passwordET = fragmentView.findViewById(R.id.password_edit_text);
         continueBtn = fragmentView.findViewById(R.id.continue_btn);
 
@@ -44,6 +47,8 @@ public class SignInFragment extends Fragment {
                 onContinueBtnClicked();
             }
         });
+
+        inputValidator = new InputValidator();
 
         return fragmentView;
     }
@@ -56,14 +61,25 @@ public class SignInFragment extends Fragment {
     }
 
     private void onContinueBtnClicked() {
+        boolean valid = true;
         String userName = uNameET.getText().toString();
         String password = passwordET.getText().toString();
 
-        ProgressDialog pd = new ProgressDialog(getActivity());
-        pd.setMessage("loading");
-        pd.setCanceledOnTouchOutside(false);
-        mViewModel.getUser(userName, password, pd);
-        pd.show();
+        if(!inputValidator.isValidUsername(userName)) {
+            valid = false;
+            uNameET.setError("Invalid username");
+        }
+        if (!inputValidator.isValidPassword(password)) {
+            valid = false;
+            passwordET.setError("Password should be 8-16 characters");
+        }
+        if (valid) {
+            ProgressDialog pd = new ProgressDialog(getActivity());
+            pd.setMessage("loading");
+            pd.setCanceledOnTouchOutside(false);
+            mViewModel.getUser(userName, password, pd);
+            pd.show();
+        }
     }
 
 }
