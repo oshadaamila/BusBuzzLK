64e102e9fe08b39006e6b40a5100c1f572ea1f2a
HeshanSudarshana <heshan.15@cse.mrt.ac.lk>
2019-08-05 21:15:21 +0530

    velocity added

--- app/src/main/java/com/crystalit/busbuzzlk/models/Bus.java
+++ app/src/main/java/com/crystalit/busbuzzlk/models/Bus.java
@@ -6,12 +6,16 @@ public class Bus {
     Double latitude,longitude;
     String routeID;
     Double bearing = 0.0;
+    private Double velocityX;
+    private Double velocityY;
 
     public Bus(String id, Double latitude, Double longitude, String routeID) {
         this.id = id;
         this.latitude = latitude;
         this.longitude = longitude;
         this.routeID = routeID;
+        this.setVelocityX(0.0);
+        this.setVelocityY(0.0);
     }
 
     public Bus(String id,Double latitude,Double longitude){
@@ -42,5 +46,22 @@ public class Bus {
 
     public void setBearing(Double bearing) {
         this.bearing = bearing;
+    }
+
+
+    public Double getVelocityX() {
+        return velocityX;
+    }
+
+    public Double getVelocityY() {
+        return velocityY;
+    }
+
+    public void setVelocityX(Double velocityX) {
+        this.velocityX = velocityX;
+    }
+
+    public void setVelocityY(Double velocityY) {
+        this.velocityY = velocityY;
     }
 }
